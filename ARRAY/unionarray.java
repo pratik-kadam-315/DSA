339ea8230bab2ffe25ee8d4ee9d77bb85e94e830
Pratik Kadam <pratikkadam4696@gmail.com>
2026-08-05 02:56:55 +0530

    union of two sorted array

---
+++ ARRAY/unionarray.java
@@ -0,0 +1,47 @@
+import java.util.*;
+class unionarray{
+    public static ArrayList<Integer> unionarray(int arr1[],int arr2[]){
+
+        int i=0;
+        int j=0;
+        ArrayList<Integer> ans=new ArrayList<>();
+
+        while(i<arr1.length && j<arr2.length){
+            if(arr1[i]<arr2[j]){
+                if(ans.size()==0 || ans.get(ans.size()-1)!=arr1[i]){
+                    ans.add(arr1[i]);
+                }
+                i++;
+            }else if(arr1[i]>arr2[j]){
+                while(ans.size()==0 || ans.get(ans.size()-1)!=arr2[j]){
+                    ans.add(arr2[j]);
+                }
+                j++;
+            }else{
+                if(ans.size()==0||ans.get(ans.size()-1)!=arr1[i]){
+                    ans.add(arr1[i]);
+                    i++;
+                    j++;
+                }
+            }
+        }
+        while(i<arr1.length){
+            if(ans.size()==0||ans.get(ans.size()-1)!=arr1[i]){
+                ans.add(arr1[i]);
+            }
+            i++;
+        }
+        while(j<arr2.length){
+            if(ans.size()==0||ans.get(ans.size()-1)!=arr2[j]){
+                ans.add(arr2[j]);
+            }
+            j++;
+        }
+        return ans;
+    }
+    public static void main(String[]args){
+        int arr1[]={1,2,3,4,5,6};
+        int arr2[]={1,2,5,7,8,9};
+        System.out.println(unionarray(arr1, arr2));
+    }
+}
