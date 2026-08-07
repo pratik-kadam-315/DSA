f9bcf0b1c7c907d93d5727381e8779707106e45a
Pratik Kadam <pratikkadam4696@gmail.com>
2026-08-07 12:00:38 +0530

    Today solve Q Leetcode NO 485

---
+++ ARRAY/maxconsecutive.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class maxconsecutive{
+    
+    public static int consecutive(int arr[]){
+        
+        int maxcon=0;
+        int count=1;
+        for(int num :arr){
+            if(num==1){
+                count++;
+                maxcon=Math.max(maxcon,count);
+            }else{
+                count=0;
+            }
+        }
+        return maxcon;
+    }
+
+    public static void main(String[]args){
+        int arr[]={1,0,1,1,1,1,0,0,0,1,1,1,1,1,1};
+        System.out.println(consecutive(arr));
+    }
+}
